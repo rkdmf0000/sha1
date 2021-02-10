49aed296dafecff1143c0f8a38f7486a5f04c50a
Taxi_tabby <rkdmf0000@gmail.com>
2021-02-10 23:20:37 +0900

    WINDOW BATCH FILE FOR TEST

---
+++ cmake-build-debug/run_it.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+echo hello : 
+hash_enc1.exe hello
+echo.
+
+echo suck : 
+hash_enc1.exe suck
+echo.
+
+echo suck you : 
+hash_enc1.exe suck you
+echo.
+
+echo 1234 : 
+hash_enc1.exe 1234
+echo.
+
+echo NULL: 
+hash_enc1.exe
+echo.
+
+pause
