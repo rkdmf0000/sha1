6dfc77fb40fee7c56db2905c50902d9a5cf2b01a
Taxi_tabby <rkdmf0000@gmail.com>
2021-02-10 22:50:00 +0900

    modularization main.cpp@2

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
     hash_enc _instance;
     _instance.init();
-    _instance.update(argv, sizeof(argc));
+    _instance.update(argv, argc);
     _instance.flush();
     std::cout << _instance.getResult();
     return 0;
